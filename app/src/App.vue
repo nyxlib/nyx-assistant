2cc7e6b7efa3db44f879bfd388b303b0108ed702
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-05-13 09:47:15 +0200

    update

--- app/src/App.vue
+++ app/src/App.vue
@@ -193,7 +193,7 @@ onMounted(() => {
 
     <!-- *********************************************************************************************************** -->
 
-    <nav class="navbar navbar-expand bg-primary border-bottom py-0">
+    <nav class="navbar navbar-expand bg-primary border py-0">
         <div class="container-fluid px-3" data-tauri-drag-region>
 
             <!-- *************************************************************************************************** -->
