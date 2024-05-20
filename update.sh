92870737fd18ee1425fca820d5d96de93f641500
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-05-20 21:26:09 +0200

    update

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 ########################################################################################################################
 
-cp ../indi-vue/src/components/controls/NavTabs.vue ./app/src/controls/
+cp ../indi-vue/src/components/ui/NavTabs.vue ./app/src/controls/
 
-cp ../indi-vue/src/components/controls/TabPane.vue ./app/src/controls/
+cp ../indi-vue/src/components/ui/TabPane.vue ./app/src/controls/
 
 cp ../indi-dashboard/app/src/plugins/dialog.js ./app/src/plugins/
 
