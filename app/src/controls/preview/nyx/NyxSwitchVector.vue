0d842cd99221f8b253411ba605d994bcfc8fedaa
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-09-05 09:11:18 +0200

    update

--- app/src/controls/preview/nyx/NyxSwitchVector.vue
+++ app/src/controls/preview/nyx/NyxSwitchVector.vue
@@ -53,7 +53,11 @@ const COLORS = {
             <div class="btn-group btn-group-sm mb-1 w-50" role="group">
 
                 <button class="btn" :class="{'btn-primary': defSwitch['$'] === 'On', 'btn-outline-secondary': defSwitch['$'] === 'Off', 'disabled': defSwitchVector['@perm'] === 'wo'}" :style="{'width': `${100.0 / props.defSwitchVector['children'].length}%`}" v-for="defSwitch in sortedDefs" :key="`${defSwitch['@name']}-${defSwitch['@rank']}`">
+
+                    <i :class="['bi', {'bi-check-square': defSwitch['$'] === 'On', 'bi-square': defSwitch['$'] === 'Off'}]" v-if="defSwitchVector['@rule'] === 'AnyOfMany'"></i>
+
                     {{ defSwitch['@label'] || defSwitch['@name'] }}
+
                 </button>
 
             </div>
