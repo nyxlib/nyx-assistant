84c946f5260f1d600a777445757c1816d84039bb
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2026-04-18 23:15:21 +0200

    update

--- src/confs/AssistantConf.vue
+++ src/confs/AssistantConf.vue
@@ -30,19 +30,25 @@ const configStore = addon.configStore();
             </div>
             <div class="card-body">
 
+                <!-- *********************************************************************************************** -->
+
                 <div class="mb-3">
                     <label for="AD718E97" class="form-label">Nyx Gen path</label>
                     <input class="form-control" type="text" placeholder="/usr/bin/nyx-gen" :disabled="!runtime.isDesktop" id="AD718E97" v-model="configStore.globals.nyx_assistant.nyx_gen_path" />
                 </div>
 
-                <div class="alert alert-primary" role="alert">
+                <!-- *********************************************************************************************** -->
+
+                <div class="alert alert-primary mt-1" role="alert">
                     <i class="bi bi-download"></i> <a href="https://github.com/nyxlib/nyx-gen" target="_blank">Download Nyx Gen.</a>
                 </div>
+
+                <!-- *********************************************************************************************** -->
 
             </div>
         </div>
 
-        <!-- *********************************************************************************************************** -->
+        <!-- ******************************************************************************************************* -->
 
     </div>
 
